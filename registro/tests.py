7cfc11e99fe0209a4e7cdb611bba22054322f65b
Neutrolex <60861853campos@gmail.com>
2025-07-24 04:15:03 -0500

    test add

--- registro/tests.py
+++ registro/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Evento
 
-# Create your tests here.
+class TestEventoDeleteView(TestCase):
+    def setUp(self):
+        self.evento = Evento.objects.create(
+            nombre="Prueba",
+            fecha="2099-12-31",
+            ubicacion="Lugar",
+            organizador="Admin"
+        )
+
+    def test_eliminar_evento(self):
+        url = reverse('evento_delete', args=[self.evento.id])
+        response = self.client.post(url)
+        self.assertRedirects(response, reverse('evento_list'))
+        self.assertFalse(Evento.objects.filter(id=self.evento.id).exists())
